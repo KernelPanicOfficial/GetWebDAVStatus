1728b10be220ad6487cd682f93e3a59b7e5b1148
Nikos Vourdas <32869078+nickvourd@users.noreply.github.com>
2025-11-27 18:56:52 -0600

    Improve WebDAV status checking logic
    Refactor memory allocation and pipe name handling for WebDAV status check.

--- GetWebDAVStatus_BOF/Source/GetWebDAVStatus.c
+++ GetWebDAVStatus_BOF/Source/GetWebDAVStatus.c
@@ -1,39 +1,117 @@
 #include <windows.h>
-#include <stdio.h>
 #include "beacon.h"
 
 WINBASEAPI BOOL WINAPI KERNEL32$WaitNamedPipeW(LPCWSTR lpNamedPipeName, DWORD nTimeOut);
-WINBASEAPI void* WINAPI MSVCRT$malloc(size_t);
-WINBASEAPI size_t WINAPI MSVCRT$wcslen(const wchar_t* str);
-WINBASEAPI wchar_t* WINAPI MSVCRT$wcscpy(wchar_t* dest, const wchar_t* source);
-WINBASEAPI wchar_t* WINAPI MSVCRT$wcscat(wchar_t* dest, const wchar_t* source);
-WINBASEAPI void WINAPI MSVCRT$free(void*);
+WINBASEAPI HANDLE WINAPI KERNEL32$GetProcessHeap(void);
+WINBASEAPI LPVOID WINAPI KERNEL32$HeapAlloc(HANDLE hHeap, DWORD dwFlags, SIZE_T dwBytes);
+WINBASEAPI BOOL WINAPI KERNEL32$HeapFree(HANDLE hHeap, DWORD dwFlags, LPVOID lpMem);
 
 void go(char* args, int length) 
 {
-    wchar_t* host;
     datap parser;
-    BeaconDataParse(&parser, args, length);
-    host = (wchar_t*)BeaconDataExtract(&parser, NULL);
-    //BeaconPrintf(CALLBACK_OUTPUT, "Value of host: %S", host);
-    
-    wchar_t* pipeNameHead = L"\\\\";
-    wchar_t* pipeNameTail = L"\\pipe\\DAV RPC SERVICE";
+    wchar_t* host = NULL;
+    wchar_t* fullPipeName = NULL;
     BOOL pipeStatus = 0;
+    HANDLE hHeap = NULL;
+    int totalHosts = 0;
+    int enabledCount = 0;
+    int disabledCount = 0;
+    int hostLen = 0;
+    int i = 0;
+    int numHosts = 0;
 
-    wchar_t* fullPipeName = (wchar_t*)MSVCRT$malloc((MSVCRT$wcslen(host) + MSVCRT$wcslen(pipeNameHead) + MSVCRT$wcslen(pipeNameTail) + 1) * sizeof(wchar_t));
-    MSVCRT$wcscpy(fullPipeName, pipeNameHead);
-    MSVCRT$wcscat(fullPipeName, host);
-    MSVCRT$wcscat(fullPipeName, pipeNameTail);
+    BeaconDataParse(&parser, args, length);
+    hHeap = KERNEL32$GetProcessHeap();
 
-    pipeStatus = KERNEL32$WaitNamedPipeW(fullPipeName, 3000);
-    if (pipeStatus == 0)
+    // First argument is the number of hosts
+    numHosts = BeaconDataInt(&parser);
+
+    if (numHosts <= 0)
     {
-        BeaconPrintf(CALLBACK_ERROR, "Unable to hit DAV pipe on %S, system is either unreachable or does not have WebClient service running", host);
+        BeaconPrintf(CALLBACK_ERROR, "[!] No targets specified");
+        return;
     }
-    else
+
+    BeaconPrintf(CALLBACK_OUTPUT, "=== GetWebDAVStatus Check ===\n");
+
+    for (totalHosts = 0; totalHosts < numHosts; totalHosts++)
     {
-        BeaconPrintf(CALLBACK_OUTPUT, "WebClient service is active on %S", host);
+        host = (wchar_t*)BeaconDataExtract(&parser, NULL);
+        
+        if (host == NULL || host[0] == L'\0')
+        {
+            break;
+        }
+
+        // Calculate length manually
+        hostLen = 0;
+        while (host[hostLen] != L'\0')
+        {
+            hostLen++;
+        }
+
+        // Allocate buffer for full pipe name
+        // \\host\pipe\DAV RPC SERVICE = 2 + hostLen + 21 + 1
+        fullPipeName = (wchar_t*)KERNEL32$HeapAlloc(hHeap, HEAP_ZERO_MEMORY, (2 + hostLen + 21 + 1) * sizeof(wchar_t));
+        
+        if (fullPipeName == NULL)
+        {
+            BeaconPrintf(CALLBACK_ERROR, "[!] Memory allocation failed for %S", host);
+            continue;
+        }
+
+        // Build pipe name manually
+        // Copy "\\\\"
+        fullPipeName[0] = L'\\';
+        fullPipeName[1] = L'\\';
+        
+        // Copy host
+        for (i = 0; i < hostLen; i++)
+        {
+            fullPipeName[2 + i] = host[i];
+        }
+        
+        // Copy "\\pipe\\DAV RPC SERVICE"
+        fullPipeName[2 + hostLen] = L'\\';
+        fullPipeName[3 + hostLen] = L'p';
+        fullPipeName[4 + hostLen] = L'i';
+        fullPipeName[5 + hostLen] = L'p';
+        fullPipeName[6 + hostLen] = L'e';
+        fullPipeName[7 + hostLen] = L'\\';
+        fullPipeName[8 + hostLen] = L'D';
+        fullPipeName[9 + hostLen] = L'A';
+        fullPipeName[10 + hostLen] = L'V';
+        fullPipeName[11 + hostLen] = L' ';
+        fullPipeName[12 + hostLen] = L'R';
+        fullPipeName[13 + hostLen] = L'P';
+        fullPipeName[14 + hostLen] = L'C';
+        fullPipeName[15 + hostLen] = L' ';
+        fullPipeName[16 + hostLen] = L'S';
+        fullPipeName[17 + hostLen] = L'E';
+        fullPipeName[18 + hostLen] = L'R';
+        fullPipeName[19 + hostLen] = L'V';
+        fullPipeName[20 + hostLen] = L'I';
+        fullPipeName[21 + hostLen] = L'C';
+        fullPipeName[22 + hostLen] = L'E';
+        fullPipeName[23 + hostLen] = L'\0';
+
+        pipeStatus = KERNEL32$WaitNamedPipeW(fullPipeName, 3000);
+        
+        if (pipeStatus == 0)
+        {
+            BeaconPrintf(CALLBACK_OUTPUT, "[-] %S - WebClient NOT running or unreachable", host);
+            disabledCount++;
+        }
+        else
+        {
+            BeaconPrintf(CALLBACK_OUTPUT, "[+] %S - WebClient ENABLED", host);
+            enabledCount++;
+        }
+
+        KERNEL32$HeapFree(hHeap, 0, fullPipeName);
+        fullPipeName = NULL;
     }
-    MSVCRT$free((void*)fullPipeName);
+
+    BeaconPrintf(CALLBACK_OUTPUT, "\n[*] Summary: %d hosts checked, %d enabled, %d disabled/unreachable", 
+                 totalHosts, enabledCount, disabledCount);
 }
